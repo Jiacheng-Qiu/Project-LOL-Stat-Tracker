4c76f82c4ad29e99f131be18566164389e9797c0
rhit-qiuj1 <qiuj1@rose-hulman.edu>
2020-10-12 21:29:17 -0400

    Initial commit with simple example

---
+++ public/scripts/main.js
@@ -0,0 +1,36 @@
+/**
+ * @fileoverview
+ * Provides the JavaScript interactions for all pages.
+ *
+ * @author 
+ * PUT_YOUR_NAME_HERE
+ */
+
+/** namespace. */
+var rhit = rhit || {};
+
+/** globals */
+rhit.variableName = "";
+
+/** function and class syntax examples */
+rhit.functionName = function () {
+	/** function body */
+};
+
+rhit.ClassName = class {
+	constructor() {
+
+	}
+
+	methodName() {
+
+	}
+}
+
+/* Main */
+/** function and class syntax examples */
+rhit.main = function () {
+	console.log("Ready");
+};
+
+rhit.main();
