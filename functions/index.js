02695e61875f7bca490597d253bf23f0e6d77cc7
Qingfeng Chai <qchai@uchicago.edu>
2020-11-02 02:00:00 -0500

    backend stuff

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,36 @@
-const functions = require('firebase-functions');
+// TODO: PROPER PLAN FOR THE FUNCTIONS AND THEIR FUNCTIONALITY
+
+const functions = require("firebase-functions");
+
+const admin = require("firebase-admin");
+admin.initializeApp();
+
+let {
+  FB_COL_SUMMONERS,
+  FB_FIELD_SUMMONER_ID,
+  FB_FIELD_SUMMONER_TIER,
+  FB_FIELD_SUMMONER_RANK,
+  FB_FIELD_SUMMONER_WINS,
+  FB_FIELD_SUMMONER_LOSSES,
+  FB_FIELD_SUMMONER_LP,
+  FB_FIELD_QUEUE_TYPE,
+  FB_FIELD_SUMMONER_LEAGUES,
+
+  FB_FIELD_TIMESTAMP,
+
+  CONST_RANKED_SOLO,
+  CONST_RANKED_FLEX,
+
+  FB_COL_USERS,
+  FB_COL_MATCHES,
+  FB_COL_LIVE_MATCHES,
+} = require("./constants");
+
+let {
+  extractKeys,
+  getSummonerByName,
+  getSummonerLeagueByID,
+} = require("./utils");
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -7,3 +39,70 @@ const functions = require('firebase-functions');
 //   functions.logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+
+/**
+ * @param  {} data      {summonerName: "FwiedWice", region: "NA1"}
+ * @param  {} context   auth context
+ *
+ * initial fetching of summoner data first time they are in the DB
+ */
+exports.getSummonerFull = functions.https.onCall(async (data, context) => {
+  let summonerName = data.summonerName;
+  let region = data.region;
+
+  let dbSummonerID = region + summonerName;
+  try {
+    let summonerInfo = await getSummonerByName(region, summonerName);
+    if (summonerInfo.err) {
+      return { err: summonerInfo.err };
+    }
+    let summonerId = summonerInfo[FB_FIELD_SUMMONER_ID];
+
+    let summonerLeagues = await getSummonerLeagueByID(region, summonerId);
+    if (summonerLeagues.err) {
+      return { err: summonerLeagues.err };
+    }
+
+    let desiredFields = [
+      FB_FIELD_SUMMONER_TIER,
+      FB_FIELD_SUMMONER_RANK,
+      FB_FIELD_SUMMONER_WINS,
+      FB_FIELD_SUMMONER_LOSSES,
+      FB_FIELD_SUMMONER_LP,
+    ];
+
+    let summonerLeaguesFiltered = {};
+
+    summonerLeagues.map((summonerLeague) => {
+      let queueType = summonerLeague[FB_FIELD_QUEUE_TYPE];
+      summonerLeaguesFiltered[queueType] = extractKeys(
+        desiredFields,
+        summonerLeague
+      );
+    });
+
+    let summoner = {
+      ...summonerInfo,
+      [FB_FIELD_SUMMONER_LEAGUES]: summonerLeaguesFiltered,
+      [FB_FIELD_TIMESTAMP]: admin.firestore.Timestamp.now(),
+    };
+
+    await admin
+      .firestore()
+      .collection(FB_COL_SUMMONERS)
+      .doc(dbSummonerID)
+      .set(summoner);
+
+    return summoner;
+  } catch (err) {
+    return { err };
+  }
+});
+
+/**
+ * @param  {} data      {summonerName: "FwiedWice", region: "NA1"}
+ * @param  {} context   auth context
+ */
+exports.retrieveProfile = functions.https.onCall(async (data, context) => {
+  // 15 second cooldown for testing
+});
--- functions/utils.js
+++ functions/utils.js
@@ -9,32 +9,56 @@ let {
 } = require("./constants");
 
 // constants
-const KEY = require("../riotkey.json")["key"];
+const KEY = require("./riotkey.json")["key"];
 const ARGS = {
   "X-Riot-Token": KEY,
 };
 
-let getSummonerByName = async (region, summonerName) => {
+/**
+ * @param  {} keys    list of keys
+ * @param  {} dict    dictionary
+ *
+ * returns a filtered version of the dictionary with only desired keys
+ */
+exports.extractKeys = (keys, dict) => {
+  res = {};
+  for (let i = 0; i < keys.length; i++) {
+    res[keys[i]] = dict[keys[i]];
+  }
+
+  return res;
+};
+/**
+ * @param  {} region          region of player (NA1, EUN1, etc...)
+ * @param  {} summonerName    summoner name of player (FwiedWice, Emperdust, etc...)
+ *
+ * retrieves basic summoner info (all kinds of IDs)
+ */
+exports.getSummonerByName = async (region, summonerName) => {
   try {
     let requestURL = `https://${region}.api.riotgames.com/lol/summoner/v4/summoners/by-name/${summonerName}`;
     let response = await req.get(requestURL).set(ARGS);
 
-    console.log(response.body);
+    return response.body;
   } catch (err) {
     console.log(err);
+    return { err: err };
   }
 };
 
-let getSummonerLeagueByID = async (region, summonerID) => {
+/**
+ * @param  {} region      region of player (NA1, EUN1, etc...)
+ * @param  {} summonerID  summoner ID of player (retrieved from riot API)
+ *
+ * retrieves info about all leagues user is in (Solo, Flex)
+ */
+exports.getSummonerLeagueByID = async (region, summonerID) => {
   try {
     let requestURL = `https://${region}.api.riotgames.com/lol/league/v4/entries/by-summoner/${summonerID}`;
     let response = await req.get(requestURL).set(ARGS);
 
-    console.log(response.body);
+    return response.body;
   } catch (err) {
-    console.log(err);
+    return { err: err };
   }
 };
-
-// getSummonerByName("NA1", "FwiedWice");
-getSummonerLeagueByID("NA1", "qf448ol_kx-u491ShYQTcCBdSTwtKtHlzHNhm3lN9OAsJ58");
