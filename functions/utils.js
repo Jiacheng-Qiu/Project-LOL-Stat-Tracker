1958c830e03bcf2fce9c46c9e1ba6c7cd61d1a8c
Qingfeng Chai <qchai@uchicago.edu>
2020-11-12 13:55:43 -0500

    oncreate acc trigger added

--- functions/utils.js
+++ functions/utils.js
@@ -69,7 +69,7 @@ exports.parseMatch = (rawMatch) => {
   let teams = rawMatch["teams"];
   teams.forEach((team) => {
     let teamID = "" + team["teamId"];
-    let win = team["win"] == "Win";
+    let win = team["win"] === "Win";
     match["teams"][teamID] = win;
   });
 
