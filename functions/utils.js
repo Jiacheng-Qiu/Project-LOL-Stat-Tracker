5558415f0c3a4e9077fad7383d7d9f544965686c
Qingfeng Chai <qchai@uchicago.edu>
2020-11-12 11:51:53 -0500

    progress

--- functions/utils.js
+++ functions/utils.js
@@ -6,6 +6,12 @@ let {
   FB_COL_USERS,
   FB_COL_MATCHES,
   FB_COL_LIVE_MATCHES,
+  FB_FIELD_SUMMONER_TIER,
+  FB_FIELD_SUMMONER_RANK,
+  FB_FIELD_SUMMONER_WINS,
+  FB_FIELD_SUMMONER_LOSSES,
+  FB_FIELD_SUMMONER_LP,
+  FB_FIELD_QUEUE_TYPE,
 } = require("./constants");
 
 // constants
@@ -13,6 +19,8 @@ const KEY = require("./riotkey.json")["key"];
 const ARGS = {
   "X-Riot-Token": KEY,
 };
+
+const MATCH_COUNT = 5;
 
 /**
  * @param  {} keys    list of keys
@@ -57,8 +65,72 @@ exports.getSummonerLeagueByID = async (region, summonerID) => {
     let requestURL = `https://${region}.api.riotgames.com/lol/league/v4/entries/by-summoner/${summonerID}`;
     let response = await req.get(requestURL).set(ARGS);
 
-    return response.body;
+    let desiredFields = [
+      FB_FIELD_SUMMONER_TIER,
+      FB_FIELD_SUMMONER_RANK,
+      FB_FIELD_SUMMONER_WINS,
+      FB_FIELD_SUMMONER_LOSSES,
+      FB_FIELD_SUMMONER_LP,
+    ];
+
+    let summonerLeaguesFiltered = {};
+
+    response.body.map((summonerLeague) => {
+      let queueType = summonerLeague[FB_FIELD_QUEUE_TYPE];
+      summonerLeaguesFiltered[queueType] = exports.extractKeys(
+        desiredFields,
+        summonerLeague
+      );
+    });
+
+    return summonerLeaguesFiltered;
   } catch (err) {
     return { err: err };
+  }
+};
+/**
+ * @param  {} region          region
+ * @param  {} accountID       accountID of  player
+ * @param  {} beginIndex=0    beginIndex of matches
+ */
+exports.getMatchesByAccountID = async (region, accountID, beginIndex = 0) => {
+  let endIndex = beginIndex + MATCH_COUNT;
+
+  try {
+    let requestURL =
+      `https://${region}.api.riotgames.com/lol/match/v4/matchlists/by-account/` +
+      `${accountID}?endIndex=${endIndex}&beginIndex=${beginIndex}`;
+    let response = await req.get(requestURL).set(ARGS);
+    return response.body;
+  } catch (err) {
+    return { err };
+  }
+};
+
+/**
+ * @param  {} region
+ * @param  {} summonerID
+ */
+exports.getSummonerLiveGameByID = async (region, summonerID) => {
+  try {
+    let requestURL = `https://${region}.api.riotgames.com/lol/spectator/v4/active-games/by-summoner/${summonerID}`;
+    let response = await req.get(requestURL).set(ARGS);
+    return response.body;
+  } catch (err) {
+    return { err };
+  }
+};
+
+/**
+ * @param  {} region
+ * @param  {} matchID
+ */
+exports.getMatchByID = async (region, matchID) => {
+  try {
+    let requestURL = `https://${region}.api.riotgames.com/lol/match/v4/matches/${matchID}`;
+    let response = await req.get(requestURL).set(ARGS);
+    return response.body;
+  } catch (err) {
+    return { err };
   }
 };
